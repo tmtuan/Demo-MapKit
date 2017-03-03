84be38a34755dea2f1d05863a1a0e26cd22218e8
mtuan <tranminhtuan684@gmail.com>
2017-03-03 09:16:32 +0700

    Initial Demo MapKit in Objective C

---
+++ DemoMapKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoMapKit
+//
+//  Created by ThaoLT on 3/3/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoMapKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoMapKit
+//
+//  Created by ThaoLT on 3/3/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
