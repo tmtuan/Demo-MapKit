e2bce4611696995b06024d64fdf25e084eedbb5b
mtuan <tranminhtuan684@gmail.com>
2017-03-04 17:15:18 +0700

    Implement viewDidLoad

--- DemoMapKit/ViewController.m
+++ DemoMapKit/ViewController.m
@@ -24,6 +24,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.mapView.delegate = self;
+    
+    // create locations
+    // Location 1
+    CLLocationCoordinate2D location1;
+    location1.latitude = 10.779784;
+    location1.longitude = 106.698995;
+    
+    
+    // location 2
+    CLLocationCoordinate2D location2;
+    location2.latitude = 10.776517;
+    location2.longitude = 106.703096;
+    
+    
+    // location 3
+    CLLocationCoordinate2D location3;
+    location3.latitude = 10.777961;
+    location3.longitude = 106.696234;
+
+    // define map span
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.028;
+    span.longitudeDelta = 0.028;
+    
+    // create region, with span and center
+    MKCoordinateRegion region;
+    region.span = span;
+    region.center = location2;
+    
+    // center the map
+    [self.mapView setRegion:region animated:YES];
+    
+    // create annotations
+    Location *annotation1 = [[Location alloc] initWithCoordinate:location1];
+    annotation1.title = @"Nhà thờ Đức Bà";
+    annotation1.subtitle = @"Notre Dame";
+    annotation1.logo = [UIImage imageNamed:@"pin"];
+    annotation1.url = [NSURL URLWithString:@"http://wwww.google.com"];
+    
+    Location *annotation2 = [[Location alloc] initWithCoordinate:location2];
+    annotation2.title = @"Nhà hát thành phố";
+    annotation2.subtitle = @"Theatre";
+    annotation2.logo = [UIImage imageNamed:@"pin"];
+    annotation2.url = [NSURL URLWithString:@"http://www.google.com"];
+    
+    Location *annotation3 = [[Location alloc] initWithCoordinate:location3];
+    annotation3.title = @"Nhà hát thành phố";
+    annotation3.subtitle = @"Theatre";
+    annotation3.logo = [UIImage imageNamed:@"pin"];
+    annotation3.url = [NSURL URLWithString:@"http://www.google.com"];
+    
+    self.annotations = [[NSArray alloc] initWithObjects:annotation1, annotation2, annotation3, nil];
+    
+    // add annotations to the map
+    [self.mapView addAnnotations:self.annotations];
+    
+    // add an overlay to the map
+    // create a circle around the center of the map (distance in meters)
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:location1 radius:2000];
+    [self.mapView addOverlay:circle];
+    
 }
 
 
@@ -55,22 +118,10 @@
 - (void)setAnnotations:(NSArray *)annotations
 {
 
-    // Location 1
-    CLLocationCoordinate2D location1;
-    location1.latitude = 10.779784;
-    location1.longitude = 106.698995;
     
     
-    // location 2
-    CLLocationCoordinate2D location2;
-    location2.latitude = 10.776517;
-    location2.longitude = 106.703096;
     
     
-    // location 3
-    CLLocationCoordinate2D location3;
-    location3.latitude = 10.777961;
-    location3.longitude = 106.696234;
     
 }
 
